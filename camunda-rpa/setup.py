da28ab80631dd65bedd5111ed019b16d83d028ec
Martin Stamm <martin.stamm@camunda.com>
2025-02-06 09:30:09 +0100

    feat: build wheel

--- camunda-rpa/setup.py
+++ camunda-rpa/setup.py
@@ -1,4 +1,8 @@
-from setuptools import setup
+import os
+from setuptools import setup, find_packages
+
+setup_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(setup_dir)
 
 setup(
    name='camunda-rpa',
@@ -6,31 +10,8 @@ setup(
    description='Exposes RPA libraries under the Camunda namespace',
    author='Camunda Services GmbH',
    author_email='info@camunda.com',
-   packages=[
-      'Camunda.Archive',
-      'Camunda.Browser.Selenium',
-      'Camunda.Calendar',
-      'Camunda.Desktop',
-      'Camunda.Desktop.Clipboard',
-      'Camunda.Desktop.OperatingSystem',
-      'Camunda.Excel.Application',
-      'Camunda.Excel.Files',
-      'Camunda.FileSystem',
-      'Camunda.FTP',
-      'Camunda.HTTP',
-      'Camunda.Images',
-      'Camunda.JavaAccessBridge',
-      'Camunda.JSON',
-      'Camunda.MFA',
-      'Camunda.MSGraph',
-      'Camunda.Outlook.Application',
-      'Camunda.PDF',
-      'Camunda.SAP',
-      'Camunda.Tables',
-      'Camunda.Tasks',
-      'Camunda.Windows',
-      'Camunda.Word.Application'
-      ], 
+   packages=find_packages(where='Camunda'), 
+   package_dir={'': 'Camunda'},
    install_requires=[
       "rpaframework",
       "rpaframework-pdf",
