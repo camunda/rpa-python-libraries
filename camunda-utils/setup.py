7e7eafd0dbe0a87996a0294a6a541a24984f6891
Martin Stamm <martin.stamm@camunda.com>
2025-04-14 12:02:26 +0200

    chore: adjust build system to pyproject.toml

--- camunda-utils/setup.py
+++ camunda-utils/setup.py
@@ -5,11 +5,5 @@ setup_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(setup_dir)
 
 setup(
-    name="camunda-utils",
-    version="9.9.9-SNAPSHOT",
-    description="Offer keywords to interact with Camunda from the RPA worker",
-    author="Camunda Services GmbH",
-    author_email="info@camunda.com",
     packages=["Camunda"],
-    install_requires=["robotframework"],
 )
